04bd8f8bcc0f05e806be0476a3435eb87b0ae57e
Shaul Ben david <Shaulbd9@gmail.com>
2022-06-09 12:51:11 +0300

    adding useContext

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,9 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
+import { UserContext } from "../../../Context/user.context";
 import './navigation.styles.scss';
 
-  const Navigation = () => {
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser);
     return (
       <Fragment>
         <div className="navigation">
--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
 import FormInput from "../form-input/form-input.component";
 import './sign-in-form.scss';
 import Button from "../button/button.component";
+import { UserContext } from "../../Context/user.context";
 
 const defaultFormField = {
     email: '',
@@ -13,6 +14,7 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormField);
     const { email, password } = formFields;
 
+    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormField);
@@ -28,8 +30,8 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
+            setCurrentUser(user);
             resetFormFields();
 
 
