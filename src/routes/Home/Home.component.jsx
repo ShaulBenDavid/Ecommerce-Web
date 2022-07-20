5939d413fcb57d3cc17b1f03e7e5400693695a2f
Shaul Ben david <Shaulbd9@gmail.com>
2022-07-20 11:34:03 +0300

    category navigate

--- src/routes/Home/Home.component.jsx
+++ src/routes/Home/Home.component.jsx
@@ -1,41 +1,12 @@
-import { useId } from 'react';
 import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 
 
 const Home = () => {
-  const categories = [
-    {
-      "id": useId(),
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": useId(),
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": useId(),
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": useId(),
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": useId(),
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
-
   return (
     <div>
         <Outlet />
-        <Directory categories={categories}/>
+        <Directory/>
     </div>
   );
 }
